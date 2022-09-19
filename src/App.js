184f3c1308b0e5f4cb744829337f532380d342d4
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-09-20 03:32:42 +0600

    load data from api with component's

--- src/App.js
+++ src/App.js
@@ -1,30 +1,63 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
  
   return (
     <div className='App'>
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
-function Counter(){
-  const [count, setCount] = useState(55)
-  const increaseCount = () =>setCount(count +1 );
-  const decreaseCount = () =>setCount(count -1 );
-  // const increaseCount = () =>{
-  //   const newCount = count + 1; 
-  //   setCount(newCount);
-  // }
+
+function ExternalUsers(){
+  const [users, setUsers] =useState([]);
+  useEffect( ()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  } ,[]);
+
   return (
     <div>
-      <h1>count : {count}</h1>
-      <button onClick={increaseCount}>Increase</button>
-      <button onClick={decreaseCount}>Increase</button>
+      <h1>External User</h1>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name = {user.name} email={user.email}></User>)
+      }
     </div>
   )
 }
+function User(props){
+  return(
+    <div style={{border: '2px solid red', margin:'20px'}}>
+      <h3>Name : {props.name}</h3>
+      <h5>Email : {props.email}</h5>
+    </div>
+  )
+}
+
+
+
+
+
+// function Counter(){
+//   const [count, setCount] = useState(55)
+//   const increaseCount = () =>setCount(count +1 );
+//   const decreaseCount = () =>setCount(count -1 );
+//   // const increaseCount = () =>{
+//   //   const newCount = count + 1; 
+//   //   setCount(newCount);
+//   // }
+//   return (
+//     <div>
+//       <h1>count : {count}</h1>
+//       <button onClick={increaseCount}>Increase</button>
+//       <button onClick={decreaseCount}>Increase</button>
+//     </div>
+//   )
+// }
 
 
 
