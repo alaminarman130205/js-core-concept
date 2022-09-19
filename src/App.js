246a74767455a35387d52a8f35bf83d030694bdf
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-09-20 00:50:05 +0600

    Count program run

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
  
   return (
-    <div>
-      
+    <div className='App'>
+      <Counter></Counter>
     </div>
   );
+}
+function Counter(){
+  const [count, setCount] = useState(55)
+  const increaseCount = () =>setCount(count +1 );
+  const decreaseCount = () =>setCount(count -1 );
+  // const increaseCount = () =>{
+  //   const newCount = count + 1; 
+  //   setCount(newCount);
+  // }
+  return (
+    <div>
+      <h1>count : {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Increase</button>
+    </div>
+  )
 }
 
 
